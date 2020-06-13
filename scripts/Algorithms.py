6cc184904d98a255041b5ab29b3da4609fa50e6f
Muhammad Salman <30803314+msalman899@users.noreply.github.com>
2020-06-13 21:19:15 +0500

    Update Algorithms.py

--- scripts/Algorithms.py
+++ scripts/Algorithms.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
 # Quick Sort Algorithm
 
-
+-- Simple
 def quicksort(arr):
     left,equal,right=[],[],[]
     key=arr[0]
@@ -44,3 +44,26 @@ if __name__ == '__main__':
     print("Given:" , arr)
     quicksort(arr)
     print("Sorted:" , arr)
+    
+-- Advanced (Lomuto partition scheme)
+
+def swap(arr,index1,index2):
+    arr[index1],arr[index2]=arr[index2],arr[index1]
+
+def quicksort_inplace(start,end):
+    global arr
+    if start < end:
+        key,i=arr[end],start
+        for j in range(start,end):
+            if arr[j] < key:
+                swap(arr,i,j)
+                i += 1
+        swap(arr,i,end)
+
+        print(*arr)
+        quicksort_inplace(start,i-1)
+        quicksort_inplace(i+1,end)
+
+n=int(input())
+arr=list(map(int,input().split()))
+quicksort_inplace(0,n-1)
